b4e05e63f44f37195431d7935a60ef1450aa0dad
Xuan Sang <nguyenxuansang10032001@gmail.com>
2025-06-11 17:04:53 +0700

    d3-3

--- src/pages/HomePage/ProductSection.jsx
+++ src/pages/HomePage/ProductSection.jsx
@@ -6,10 +6,10 @@ const ProductSection = () => {
     <div className='bg-[#FDFDFD]'>
       <div className='max-w-screen-2xl w-full px-6 lg:px-20  py-12 mx-auto'>
         <div className='flex flex-row justify-between mb-9 items-center'>
-          <div className='font-penumbra font-bold text-4xl text-primary-600'>
+          <div className='font-penumbra font-bold text-2xl lg:text-3xl xl:text-4xl text-primary-600'>
             Sản phẩm
           </div>
-          <div className='text-primary-900 text-lg font-medium cursor-pointer'>
+          <div className='text-primary-900 text-base lg:text-lg font-medium cursor-pointer'>
             Xem thêm
           </div>
         </div>
