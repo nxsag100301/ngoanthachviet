eae28de20e0f23732b8c664f1639fb6ae21330c9
Xuan Sang <nguyenxuansang10032001@gmail.com>
2025-06-12 16:40:59 +0700

    d4-2

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -40,44 +40,43 @@ const ProductCard = ({
         src='/assets/images/logo.png'
         className='absolute h-[76px] w-[122px] object-cover top-0 left-1/2 -translate-x-1/2 '
       />
-      <div className='mx-8'>
+      <div className='mx-2 md:mx-4 lg:mx-8'>
         <Status />
-      </div>
-
-      <div className='text-lg tracking-[0.15px] font-medium mx-8 mb-2'>
-        Đá cảnh mây vờn núi
-      </div>
-      {isAuction && auctionStatus ? (
-        <div>
-          <div className='w-[137px] mx-8 mb-4'>
-            <AuctionStatus auctionStatus={auctionStatus} />
-          </div>
-          <div className='mx-8 mb-4'>
-            <AuctionStartOrEnd auctionTime={auctionTime} />
-          </div>
+        <div className='text-lg tracking-[0.15px] font-medium  mb-2'>
+          Đá cảnh mây vờn núi
         </div>
-      ) : (
-        <div className='text-sm tracking-[0.15px] text-[rgba(74,76,86,1)] mx-8 mb-4'>
-          Ngang 16cm, cao 21cm, sâu 7cm
-        </div>
-      )}
-      <div className='mx-8 flex flex-row justify-between items-center text-lg tracking-[0.15px] font-medium'>
-        {isAuction === true && auctionStatus === 'underAuction' ? (
-          <Button variant='outline' className='h-[50px] w-[162px]'>
-            Đấu giá ngay
-          </Button>
-        ) : isAuction === true && auctionStatus === 'notAuctionced' ? (
-          <Button variant='outline' className='h-[50px] w-[134px]'>
-            Chi tiết
-          </Button>
+        {isAuction && auctionStatus ? (
+          <div>
+            <div className='w-[137px]  mb-4'>
+              <AuctionStatus auctionStatus={auctionStatus} />
+            </div>
+            <div className=' mb-4'>
+              <AuctionStartOrEnd auctionTime={auctionTime} />
+            </div>
+          </div>
         ) : (
-          <>
-            <span>1.111.111 VND</span>
-            <Button variant='outline' className='h-[50px] w-[134px]'>
-              Mua ngay
-            </Button>
-          </>
+          <div className='text-sm tracking-[0.15px] text-[rgba(74,76,86,1)] mb-4'>
+            Ngang 16cm, cao 21cm, sâu 7cm
+          </div>
         )}
+        <div className='flex flex-row justify-between items-center text-lg tracking-[0.15px] font-medium'>
+          {isAuction === true && auctionStatus === 'underAuction' ? (
+            <Button variant='outline' className='h-[50px] w-[162px]'>
+              Đấu giá ngay
+            </Button>
+          ) : isAuction === true && auctionStatus === 'notAuctionced' ? (
+            <Button variant='outline' className='h-[50px] w-[134px]'>
+              Chi tiết
+            </Button>
+          ) : (
+            <>
+              <span>1.111.111 VND</span>
+              <Button variant='outline' className='h-[50px] w-[134px]'>
+                Mua ngay
+              </Button>
+            </>
+          )}
+        </div>
       </div>
     </div>
   )
