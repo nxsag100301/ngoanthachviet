6b7bf97e0881e1eac12c4626b14057f638b34a78
Xuan Sang <nguyenxuansang10032001@gmail.com>
2025-06-19 14:56:03 +0700

    d8-4

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -4,11 +4,11 @@ const Status = () => {
   return (
     <div
       className='bg-[#EEFFE2] rounded-2xl p-2 sm:p-4 h-[48px] xl:h-[62px] mb-4 text-[#009800]
-       font-medium text-xs md:text-sm tracking-[0.15px] flex flex-row items-center justify-center gap-1 sm:gap-2 xl:gap-4'
+       font-medium text-xs md:text-sm tracking-[0.15px] flex flex-row items-center justify-center text-center gap-0.5 sm:gap-2 xl:gap-4'
     >
       <img
         src='/assets/icons/checked.png'
-        className='w-7 h-7 tracking-[0.15px]'
+        className='w-5 h-5 sm:w-7 sm:h-7 tracking-[0.15px]'
       />
       Đã được định danh bởi Ngoạn Thạch Việt
     </div>
