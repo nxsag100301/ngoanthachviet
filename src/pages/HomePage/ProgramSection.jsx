ea8b26ce38829a5c89bbcc05158834344ef6bb61
Xuan Sang <nguyenxuansang10032001@gmail.com>
2025-06-24 09:06:08 +0700

    version-4

--- src/pages/HomePage/ProgramSection.jsx
+++ src/pages/HomePage/ProgramSection.jsx
@@ -4,12 +4,12 @@ import { motion, AnimatePresence } from 'framer-motion'
 
 const images = [
   {
-    bgImage: '/assets/images/program/1.jpg',
-    imgSrc: '/assets/images/program/1.jpg'
+    bgImage: '/assets/images/program/1.webp',
+    imgSrc: '/assets/images/program/1.webp'
   },
   {
-    bgImage: '/assets/images/program/2.jpg',
-    imgSrc: '/assets/images/program/2.jpg'
+    bgImage: '/assets/images/program/2.webp',
+    imgSrc: '/assets/images/program/2.webp'
   },
   {
     bgImage: '/assets/images/program/3.jpg',
@@ -46,14 +46,15 @@ const ProgramCard = ({ bgImage, imgSrc, onLoad }) => {
   }
 
   return (
-    <div className='relative max-w-[1376px] mx-auto h-[300px] sm:h-[400px] md:h-[550px] xl:h-[710px] rounded-3xl'>
+    <div className='relative max-w-[1500px] mx-auto h-[300px] sm:h-[400px] md:h-[550px] xl:h-[710px] rounded-3xl'>
       {/* Nền blur */}
       <div
-        className='absolute bg-no-repeat bg-cover inset-0 z-0 w-full h-full rounded-[40px] overflow-hidden transition-opacity duration-500'
+        className='absolute bg-no-repeat bg-center bg-[#fdfdfd] border-[#fdfdfd] inset-0 z-0 w-full h-full rounded-[80px] md:rounded-[120px] overflow-hidden transition-opacity duration-500'
         style={{
           backgroundImage: `url('${bgImage}')`,
-          filter: 'blur(8px)',
-          opacity: loaded ? 1 : 0
+          filter: 'blur(4px)',
+          opacity: loaded ? 1 : 0,
+          backgroundSize: '91% 90%'
         }}
       ></div>
 
@@ -61,7 +62,7 @@ const ProgramCard = ({ bgImage, imgSrc, onLoad }) => {
       {loaded && (
         <img
           src='/assets/images/logo.png'
-          className='absolute object-contain h-[65px] sm:h-[85px] md:h-[100px] xl:h-[130px] left-1/2 -translate-x-1/2 top-0 xl:top-2
+          className='absolute object-contain h-[60px] sm:h-[85px] md:h-[100px] xl:h-[130px] left-1/2 -translate-x-1/2 top-2 xl:top-4
     transition-opacity duration-500 opacity-100'
         />
       )}
@@ -71,7 +72,7 @@ const ProgramCard = ({ bgImage, imgSrc, onLoad }) => {
         <img
           src={imgSrc}
           onLoad={handleImageLoad}
-          className={`object-cover h-[200px] sm:h-[240px] md:h-[370px] xl:h-[434px] rounded-3xl 
+          className={`object-cover h-[180px] sm:h-[240px] md:h-[370px] xl:h-[434px] rounded-2xl 
             absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2
             transition-opacity duration-500 ${
               loaded ? 'opacity-100' : 'opacity-0'
@@ -98,7 +99,7 @@ const ProgramSection = () => {
   }, [index, canSwitch])
 
   return (
-    <div className='bg-[#FDFDFD] relative pt-4 pb-16 px-6 lg:px-20'>
+    <div className='bg-[#FDFDFD] relative pt-4 pb-16 px-0 lg:px-11'>
       <div className='w-full text-center relative select-none mb-16 max-w-screen-2xl mx-auto z-20'>
         <div className='font-aquarelle text-[22px] sm:text-[32px] text-[#616161] font-normal lowercase'>
           Chương trình
